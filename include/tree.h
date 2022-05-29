a05c3f3b9345d34d5d0715d3308051e3a7872611
romanakimkin <78901819+romanakimkin@users.noreply.github.com>
2022-05-30 00:47:41 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,6 +1,8 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+
+
 class Tree {
  private:
   struct Node {
@@ -13,7 +15,12 @@ class Tree {
     if (!root->leaf.size()) {
       s += root->value;
       Perm.push_back(s);
-  class Tree {
+      return;
+    }
+  if (root->value != '*') {//корень
+    s += root->value;
+  }
+  for (size_t i = 0; i < root->leaf.size(); ++i) {
     Permutation(root->leaf[i], s);
   }
 }
@@ -21,7 +28,20 @@ void constructTree(Node* root, std::vector<char> path) {
   if (!path.size()) {
     return;
   }
- void constructTree(Node* root, vector<char> path) {
+  if (root->value != '*') {
+  for (auto i = path.begin(); i != path.end(); ++i) {
+    if (*i == root->value) {
+      path.erase(i);
+      break;
+    }
+  }
+  }
+  for (size_t i = 0; i < path.size(); ++i) {
+    root->leaf.push_back(new Node);
+  }
+  for (size_t i = 0; i < root->leaf.size(); ++i) {
+    root->leaf[i]->value = path[i];
+  }
   for (size_t i = 0; i < root->leaf.size(); ++i) {
     constructTree(root->leaf[i], path);
   }
